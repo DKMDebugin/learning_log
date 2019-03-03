64b347d78cc84292251bbd34a3ce90eb353458bc
Jamiu Olashile Salimon <salimonjamiu@yahoo.com>
2019-03-03 04:08:16 +0100

    Add more functionality

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,85 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 
 def index(request):
     """The home page for learning_log"""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """show all topics"""
+
+    # query db
+    topics = Topic.objects.order_by('date_added') # query db & sort result by date_added
+    # build dictionary
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    '''Show a single topic and all its entries'''
+    # query db
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added') # sort by date_added in reverse due to the minus sign
+
+    # build dictionary
+    context = {'topic': topic, 'entries': entries}
+
+    return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    '''Add a new topic'''
+    if request.method != 'POST':
+        # No data submitted; create a blank form
+        form = TopicForm()
+    else:
+        # POST data submitted; process data
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    '''Add a new entry for a particular topic'''
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # No data submitted create a blank form
+        form = EntryForm()
+    else:
+        # POST data submitted; process data
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+
+            new_entry.save()
+            print("REGEX ERROR!!!!!!!!!!!!!!!!!!!!!!!!!!")
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
+
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    """Edit an existing entry."""
+    entry = Entry.objects.get(id=entry_id)
+    topic =  entry.topic
+
+    if request.method != 'POST':
+        # Initial request; pre-fill form with the current entry
+        form = EntryForm(instance=entry)
+    else:
+        # POST data submitted; process data
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic',
+                                        args=[topic.id]))
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
