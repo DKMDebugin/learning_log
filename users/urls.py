ca6ee55e71189f8b077a5787aa65eb02f55c8aec
Jamiu Olashile Salimon <salimonjamiu@yahoo.com>
2019-03-05 13:26:58 +0100

    Add user login, logout & registration functionality to the users app

--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,15 @@
 
 from django.conf.urls import url
 from django.contrib.auth.views import LoginView
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
     # Login page
-    url(r'^login/$', LoginView, {'template_name': 'users/login.html'}, name='login'),
+    path(r'login/', LoginView.as_view(template_name= 'users/login.html'), name='login'),
+    #Log out url
+    path(r'logout/', views.logout_view, name='logout'),
+    # Registration Page
+    path(r'register/', views.register, name='register'),
 ]
