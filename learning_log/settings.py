41fa055b2a181d908d323e4a627fef2955ee67ed
dkmdebugin <salimonjamiu@yahoo.com>
2019-03-07 18:07:39 +0100

    Add cutom 404 and 505 error pages.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2om)u(2k$s3)_cusnc)0d^8zkq0&*pf*k$(5kk#t97$qgp%at8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'learning_log.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'learning_log/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
